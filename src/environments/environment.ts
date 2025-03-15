8792300c6e809750707bb7470e5023cb3333cd23
M4ttzz <matheusssscf@gmail.com>
2025-03-15 12:47:55 -0300

    comit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
+    apiKey: "AIzaSyDDCKtWcZa77yDzcOiDoj4j3dmRsOzHEAo",
+    authDomain: "teste-app-9743c.firebaseapp.com",
+    projectId: "teste-app-9743c",
+    storageBucket: "teste-app-9743c.firebasestorage.app",
+    messagingSenderId: "710265890296",
+    appId: "1:710265890296:web:4de2ffc5252d43dc39e20d"
   }
  };
 /*
